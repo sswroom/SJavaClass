d3c54bf8f2db0ab9c8cac6d886b38b59a6b3f024
Simon Wong <simon.wong@stone-road.net>
2021-02-24 18:08:14 +0800

    Enhance StringUtil

--- src/main/java/org/sswr/util/data/StringUtil.java
+++ src/main/java/org/sswr/util/data/StringUtil.java
@@ -2,6 +2,7 @@ package org.sswr.util.data;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Iterator;
 
 public class StringUtil
 {
@@ -22,6 +23,66 @@ public class StringUtil
 				return false;
 		}
 		return true;
+	}
+
+	public static boolean isNullOrEmpty(String s)
+	{
+		return s == null || s.length() == 0;
+	}
+
+	public static boolean hasChars(String s)
+	{
+		return s != null && s.length() > 0;
+	}
+
+	public static String leftPad(String s, int minLeng, char padChar)
+	{
+		int l = s.length();
+		if (l >= minLeng)
+		{
+			return s;
+		}
+		StringBuilder sb = new StringBuilder(minLeng);
+		l = minLeng - l;
+		while (l-- > 0)
+		{
+			sb.append(padChar);
+		}
+		sb.append(s);
+		return sb.toString();
+	}
+
+	public static String join(Iterable<String> strs, String seperator)
+	{
+		StringBuilder sb = new StringBuilder();
+		Iterator<String> it = strs.iterator();
+		if (it.hasNext())
+		{
+			sb.append(it.next());
+		}
+		while (it.hasNext())
+		{
+			sb.append(seperator);
+			sb.append(it.next());
+		}
+		return sb.toString();
+	}
+
+	public static <T extends Object> String joinObjs(Iterable<T> objs, String seperator)
+	{
+		StringBuilder sb = new StringBuilder();
+		Iterator<T> it = objs.iterator();
+		if (it.hasNext())
+		{
+			sb.append(it.next().toString());
+		}
+		while (it.hasNext())
+		{
+			sb.append(seperator);
+			sb.append(it.next().toString());
+		}
+		return sb.toString();
+		
 	}
 
 	public static Timestamp toTimestamp(String s)
