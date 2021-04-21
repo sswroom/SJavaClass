6a35dafb7d75b3622f665848feb0103737e7581d
Simon Wong <sswroom@yahoo.com>
2021-04-21 18:17:24 +0800

    Added domain user name handling

---
+++ src/main/java/org/sswr/util/io/DomainUser.java
@@ -0,0 +1,21 @@
+package org.sswr.util.io;
+
+public class DomainUser
+{
+	public static String removeUserDomain(String username)
+	{
+		int i;
+		if ((i = username.indexOf("\\")) >= 0)
+		{
+			return username.substring(i + 1);
+		}
+		else if ((i = username.indexOf("@")) >= 0)
+		{
+			return username.substring(0, i);
+		}
+		else
+		{
+			return username;
+		}
+	}
+}
