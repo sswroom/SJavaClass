5a5a9d2f337864386b356445d0b985f67384f8c7
Simon Wong <sswroom@yahoo.com>
2022-05-06 18:46:24 +0800

    Enhance DNS getting in windows

--- src/main/java/org/sswr/util/net/WindowsSocketFactory.java
+++ src/main/java/org/sswr/util/net/WindowsSocketFactory.java
@@ -1,14 +1,36 @@
 package org.sswr.util.net;
 
+import java.lang.reflect.Method;
 import java.net.InetAddress;
+import java.util.List;
 
 public class WindowsSocketFactory implements SocketFactory
 {
 	@Override
 	public InetAddress[] getDefDNS()
 	{
-		String servers = System.getProperty("sun.net.spi.nameservice.nameservers");
-		System.out.println(servers);
+		try
+		{
+			Class<?> cls = Class.forName("sun.net.dns.ResolverConfiguration");
+			Method method = cls.getMethod("open");
+			Object o = method.invoke(null);
+			Class<?> ocls = o.getClass();
+			Method nameservers = ocls.getMethod("nameservers");
+			Object nsVal = nameservers.invoke(o);
+			@SuppressWarnings("unchecked")
+			List<String> nsList = (List<String>)nsVal;
+			InetAddress[] ret = new InetAddress[nsList.size()];
+			int i = ret.length;
+			while (i-- > 0)
+			{
+				ret[i] = InetAddress.getByName(nsList.get(i));
+			}
+			return ret;
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
 		return new InetAddress[0];
 	}
 	
