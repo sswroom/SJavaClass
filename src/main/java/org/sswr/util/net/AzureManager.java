6a9246906d68d690b5130b31a89112fe4a9e37fb
Simon Wong <sswroom@yahoo.com>
2023-09-20 22:33:14 +0800

    Enhance Azure Token parsing

--- src/main/java/org/sswr/util/net/AzureManager.java
+++ src/main/java/org/sswr/util/net/AzureManager.java
@@ -11,16 +11,21 @@ import org.sswr.util.data.JSONArray;
 import org.sswr.util.data.JSONBase;
 import org.sswr.util.data.JSONObject;
 import org.sswr.util.data.textbinenc.Base64Enc;
+import org.sswr.util.io.LogLevel;
+import org.sswr.util.io.LogTool;
 
 public class AzureManager
 {
 	private boolean debug = true;
+	private LogTool errLog;
 	private Map<String, String> keyMap;
 	private SocketFactory sockf;
 	private SSLEngine ssl;
 
 	public AzureManager()
 	{
+		this.errLog = null;
+		if (debug) this.errLog = new LogTool().addPrintLog(System.out, LogLevel.RAW);
 		this.sockf = null;
 		this.ssl = null;
 		this.keyMap = null;
@@ -28,9 +33,16 @@ public class AzureManager
 
 	public AzureManager(SocketFactory sockf, SSLEngine ssl)
 	{
+		this.errLog = null;
+		if (debug) this.errLog = new LogTool().addPrintLog(System.out, LogLevel.RAW);
 		this.sockf = sockf;
 		this.ssl = ssl;
 		this.keyMap = null;
+	}
+
+	public void setErrorLog(LogTool log)
+	{
+		this.errLog = log;
 	}
 
 	public MyX509Key createKey(String kid)
@@ -80,7 +92,7 @@ public class AzureManager
 		JWToken jwt = JWToken.parse(token, null);
 		if (jwt == null)
 		{
-			if (debug) System.out.println("Error in parsing token");
+			if (errLog != null) errLog.logMessage("Error in parsing token", LogLevel.ERROR);
 			return null;
 		}
 		else
@@ -90,24 +102,24 @@ public class AzureManager
 				JSONBase json = JSONBase.parseJSONStr(jwt.getHeader());
 				if (json == null)
 				{
-					if (debug) System.out.println("Error in parsing header");
+					if (errLog != null) errLog.logMessage("Error in parsing header", LogLevel.ERROR);
 					return null;
 				}
 				String kid = json.getValueString("kid");
 				if (kid == null)
 				{
-					if (debug) System.out.println("kid not found in header");
+					if (errLog != null) errLog.logMessage("kid not found in header", LogLevel.ERROR);
 					return null;
 				}
 				MyX509Key key = createKey(kid);
 				if (key == null)
 				{
-					if (debug) System.out.println("key not found in header");
+					if (errLog != null) errLog.logMessage("key not found in header", LogLevel.ERROR);
 					return null;
 				}
 				if (!jwt.signatureValid(key.getASN1Buff(), 0, key.getASN1BuffSize(), key.getKeyType()))
 				{
-					if (debug) System.out.println("Signature not valid");
+					if (errLog != null) errLog.logMessage("Signature not valid", LogLevel.ERROR);
 					return null;
 				}
 			}
@@ -115,14 +127,14 @@ public class AzureManager
 			Map<String, String> result = jwt.parsePayload(param, false, null);
 			if (result == null)
 			{
-				if (debug) System.out.println("Error in parsing payload");
+				if (errLog != null) errLog.logMessage("Error in parsing payload", LogLevel.ERROR);
 				return null;
 			}
 			if (!ignoreTimeCheck)
 			{
 				if (param.isExpired(System.currentTimeMillis()))
 				{
-					if (debug) System.out.println("Expired");
+					if (errLog != null) errLog.logMessage("Expired", LogLevel.ERROR);
 					return null;
 				}
 			}
